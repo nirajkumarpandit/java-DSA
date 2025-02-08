11119fc6dbb2b921efa4c1d2efcee232fd39d64c
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-02-08 22:13:44 +0530

    binary to decimal & decimal to binary

--- function/binaryTodecimal.java
+++ function/binaryTodecimal.java
@@ -1,6 +1,8 @@
 package function;
 
 public class binaryTodecimal {
+    // binary to decimal conversion
+
     public static void binTodecimal(int bin){
         int pow=0;
         int decimal=0;
@@ -14,7 +16,21 @@ public class binaryTodecimal {
         System.out.println("decimal of "+binary+" = "+ decimal);
 
     }
+    // decimal to binary 
+    public static void decimalTObinary(int n){
+        int mynumber =n;
+        int pow=0;
+        int binary=0;
+        while (n>0) {
+            int reminder=n%2;
+            binary =binary+(reminder* (int)Math.pow(10, pow));
+            pow++;
+            n /=2;
+        }
+        System.out.println("binary of "+ mynumber +" ="+ binary);
+
+    }
     public static void main(String[] args) {
-        binTodecimal(101);
+        decimalTObinary(7);
     }
 }
