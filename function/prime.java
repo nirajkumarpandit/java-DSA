64019892ff1ab34f9ed7c3a12c7422aa07839004
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-02-08 21:17:54 +0530

    prime number

---
+++ function/prime.java
@@ -0,0 +1,23 @@
+package function;
+import java.util.*;
+public class prime {
+    public static boolean isPrime(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n=sc.nextInt();
+        System.out.println(isPrime(n));
+        sc.close();
+    }
+}
