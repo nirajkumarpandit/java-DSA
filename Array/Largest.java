df6f69f4a73a9320b512d3f06f24dd664c234c2f
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-02-12 18:56:36 +0530

    largest number

---
+++ Array/Largest.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Largest {
+    public static int  big(int arr[]){
+     int largest=Integer.MIN_VALUE;
+     for(int i=0; i<arr.length; i++){
+        if(largest<arr[i]){
+            largest=arr[i];
+        }
+     }
+     return largest;
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,800,9};
+        System.out.println(big(arr));
+    }
+}
