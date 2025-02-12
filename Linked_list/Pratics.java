51d627e76e6c98b49a98810547701c3d5a595fba
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-02-12 10:11:17 +0530

    Linked list insert at head and tail

--- Linked_list/Pratics.java
+++ Linked_list/Pratics.java
@@ -1,6 +1,6 @@
 package Linked_list;
 
-public class LinkedList {
+public class Pratics {
     public static class Node{
         int data;
         Node next;
@@ -12,20 +12,19 @@ public class LinkedList {
     static Node head;
     static Node tail;
     public void addFirst(int data){
-        // step 1 create a node 
+        // step 1 create a node
         Node newNode=new Node(data);
         if(head==null){
             head=tail=newNode;
             return;
         }
-        //step 2 new node point to head
-         newNode.next=head;
-         //step 3 head change
-         head=newNode;
-         
+        // step 2 newNodeka next head ko point kare
+        newNode.next=head;
+        // step 3 newNode ko head bana do
+        head=newNode;
     }
-    public void addLast(int data){
-        //step 1 create a node 
+    public  void addlast(int data){
+        // step 1 create a node 
         Node newNode=new Node(data);
         if(head==null){
             head=tail=newNode;
@@ -33,27 +32,22 @@ public class LinkedList {
         }
         //step 2 tail ka next newNode ko point kare
         tail.next=newNode;
-        //step 3 tail ab newNode ko point karega
         tail=newNode;
     }
     public void print(){
-        if(head==null){
-            System.out.print("null");
-        }
-        Node temp=head;
-        while(temp !=null){
+        Node temp=head;  // temparary node jo head per hai
+        while(temp!=null){
             System.out.print(temp.data+" ");
-            temp=temp.next;
+            temp=temp.next;   // temp ka next node
         }
-
     }
-
     public static void main(String[] args) {
-        LinkedList ll=new LinkedList();
-        ll.addFirst(1);
-        ll.addLast(2);
-        ll.addLast(3);
-        ll.print();
-        
+        Pratics pp=new Pratics();
+        pp.addFirst(3);
+        pp.addlast(4);
+        pp.addFirst(9);
+        pp.addlast(8);
+        pp.print();
+
     }
 }
