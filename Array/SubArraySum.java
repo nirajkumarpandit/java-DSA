2c8cb8ede178d721c023e421bf70559750593962
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-03-01 16:47:19 +0530

    Subarray Sum

--- Array/SubArraySum.java
+++ Array/SubArraySum.java
@@ -1,14 +1,14 @@
 package Array;
 
 public class SubArraySum {
-    public static void maxSum(int arr[]){
-        int maxSum=Integer.MIN_VALUE;
+
+    // brout force approach
+    public static void maxSum(int arr[]){    // time complexity O(n^3)
+        int maxSum=Integer.MIN_VALUE;         // space complexity O(1)
         for(int i=0; i<arr.length; i++){
-            int current=0;
-            int start=i;
-            for(int j=i; j<arr.length; j++){
-                int end=j;
-                for(int k=start; k<=end; k++){
+            for(int j=i; j<arr.length; j++){ //for start 
+                int current=0;
+                for(int k=i; k<=j; k++){   // for end
                     current +=arr[k];
                 }
                 if(maxSum<current){
@@ -18,8 +18,27 @@ public class SubArraySum {
         }
         System.out.println(maxSum);
     }
+    public static void maxSubArraySum(int arr[]){
+        
+    }
+    // optimal approach
+    // kadane algorithm
+    public static void kadane_s(int arr[]){
+        int cs=0;
+        int ms=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            cs=cs+arr[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms=Math.max(cs, ms);
+        }
+        System.out.println(ms);
+    }
     public static void main(String[] args){
-        int arr[]={1,3,4,5,6};
+        int arr[]={1,3,-4,5,-6};
         maxSum(arr);
+        System.out.println("kadane_s algorithm :");
+        kadane_s(arr);
     }
 }
---
+++ pratics/P1.java
@@ -0,0 +1,65 @@
+package pratics;
+import java.util.*;
+public class P1{
+    // binary search 
+    public static int BinSearch(int arr[], int target){
+        int st=0;
+        int end=arr.length-1;
+        while (st<=end) {
+            int mid=(st+end)/2;
+            if(arr[mid]==target){
+                
+                return mid;
+            }
+            if(arr[mid]<target){
+               st= mid+1;
+            }else{
+                end=mid--;
+            }
+        }
+        return -1;
+    }
+    // input from user 
+    public static void inputArray(int arr[]){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+    // find largest element in array
+    public static void largest(int arr[]){
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        System.out.println("The maximum value :"+max);
+    }
+    // minimum value in array
+    public static void minimum(int arr[]){
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(min>arr[i]){
+                min=arr[i];
+            }
+        }
+        System.out.println("The minimum value :"+min);
+    }
+    // print function of array
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[4];
+        System.out.println("Enter the array element :");
+        inputArray(arr);
+        largest(arr);
+        minimum(arr);
+        int target=6;
+        System.out.println(BinSearch(arr, target));
+    }
+   
+}
