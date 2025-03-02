7e953632b8878146c918d9cde240002831a0c91d
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-03-02 10:40:11 +0530

    Total trapped water

---
+++ Array/TrappingRainwater.java
@@ -0,0 +1,60 @@
+package Array;
+
+public class TrappingRainwater {
+  public static int trappedwater(int height[]){
+    // calculate left max boudry
+    int n=height.length;
+    int leftMax[]=new int[n];
+    leftMax[0]=height[0];
+    for(int i=1; i<n; i++){
+        leftMax[i]=Math.max(height[i], leftMax[i-1]);
+    }
+    // calculate right max boundry
+    int rightMax[]=new int[n];
+    rightMax[n-1]=height[n-1];
+    for(int i=n-2; i>=0; i--){
+        rightMax[i]=Math.max(height[i],rightMax[i+1]);  // axuilary arry to store the max boubdry 
+    }
+    // calculate water level and trapped water
+    int trappedWater=0;
+    for(int i=0; i<n; i++){
+        int waterLevel =Math.min(leftMax[i], rightMax[i]);
+        trappedWater +=waterLevel - height[i];
+    }
+    return trappedWater;
+  }
+    public static void main(String[] args) {
+        int height[]={4,2,0,6,3,2,5};
+        System.out.println(trappedwater(height));
+        
+    }
+}
+ 
+// package Array;
+// public class TrappingRainwater {
+//     public static int rainWater(int height[]){
+//         // calculate left max boundry
+//         int leftMax[]=new int[height.length];
+//         leftMax[0]=height[0];
+//         for(int i=1; i<height.length; i++){
+//             leftMax[i]=Math.max(height[i], leftMax[i-1]);
+//         }
+//         // calculate right max boundry
+//         int rightMax[]=new int[height.length];
+//         rightMax[height.length-1]=height[height.length-1];
+//         for(int i=height.length-2; i>=0; i--){
+//             rightMax[i]=Math.max(height[i], rightMax[i+1]);
+//         }
+//         //calculate water level and total watertrapped
+//         int watertrapped=0;
+//         for(int i=0; i<height.length; i++){
+//             int waterLevel=Math.min(leftMax[i], rightMax[i]);
+//             watertrapped += waterLevel-height[i];
+//         }
+//         return watertrapped;
+//     }
+//     public static void main(String[] args) {
+//         int height[]={4,2,0,6,3,2,5};
+//         System.out.println( rainWater(height));
+//     }
+// }
