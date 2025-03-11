78d5e8f5c3b62b6f8e14809768c46383b219b7d6
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-03-11 23:08:09 +0530

    rotate the element by the give number of element is rotated

--- Array/LeftRotate.java
+++ Array/LeftRotate.java
@@ -9,6 +9,27 @@ public class LeftRotate {
         arr[arr.length-1]=temp;
 
     }
+    //*** if give the no. of element are shifted  */
+    // Brute force approach
+     static void leftRotate(int arr[], int d){
+        // calculate the no. of rotation 
+        d= d%arr.length;
+        // create a temp array and store the elements are rotated
+        int temp[]=new int[d];
+        for(int i=0; i<d; i++){
+            temp[i]=arr[i];
+        }
+        // shifting the array element
+        for(int i=d; i<arr.length; i++ ){
+            arr[i-d]=arr[i];
+        }
+        // put back the temp element in the array in vacent position
+        // vacent position = size of array - d
+        for(int i= arr.length-d; i<arr.length; i++){
+            arr[i]=temp[i-(arr.length-d)];
+        }
+     }
+
     // for print the array
     static void print(int arr[]){
         for(int i=0; i<arr.length; i++){
@@ -16,8 +37,9 @@ public class LeftRotate {
         }
     }
     public static void main(String[] args){
-        int arr[]={1,2,3,4,5};
-        leftRotate(arr);
+        int arr[]={1,2,3,4,5,6,7};
+        // leftRotate(arr);
+        leftRotate(arr, 3);
         print(arr);
     }
 }
