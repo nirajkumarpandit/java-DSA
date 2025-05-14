59333ae51e03075ae1b87fa54afaf62cd9b1fa53
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2025-05-14 10:55:20 +0530

    fibonacci series

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -15,7 +15,10 @@ public class Fibonacci {
     }
 
     public static void main(String args[]) {
-        int n = 3;
-        System.out.print(fib(n));
+        int n = 10;
+         System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
     }
 }
