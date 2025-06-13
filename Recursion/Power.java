750a066cc279f82d7a75b64fb8adb6e030b3ec2d
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2025-06-13 06:57:55 +0530

    friend pairing problem

--- Recursion/Power.java
+++ Recursion/Power.java
@@ -2,36 +2,37 @@ package Recursion;
 
 public class Power {
     // print x^n in Time complexity of O(n)
-    public static int pow(int x, int n){
+    public static int pow(int x, int n) {
         // base case
-        if(n==0){
+        if (n == 0) {
             return 1;
         }
         // calculate pow(x, n-1)
         // int pnm1 =pow(x, n-1);
         // int xn=x*pnm1;
         // return xn;
-         return x*pow(x, n-1);
+        return x * pow(x, n - 1);
     }
 
     // print x^n in time complexity of O(logn)
 
-    public static int power(int n, int x){
+    public static int power(int n, int x) {
         // base case
-        if(n==0){
+        if (n == 0) {
             return 1;
         }
         // calculate half power
-        int halfPower=power(n/2, x);
-        int halfPowerSq= halfPower * halfPower;
+        int halfPower = power(n / 2, x);
+        int halfPowerSq = halfPower * halfPower;
         // if n is odd
-        if(n%2!=0){
-            halfPowerSq= x*halfPowerSq;
+        if (n % 2 != 0) {
+            halfPowerSq = x * halfPowerSq;
         }
         return halfPowerSq;
     }
-    public static void main(String args[]){
-        //  System.out.println(pow(2, 5));
-         System.out.println(power(5, 2));
+
+    public static void main(String args[]) {
+        // System.out.println(pow(2, 5));
+        System.out.println(power(5, 2));
     }
 }
