69eca9fa94dbb0588ff7a366c1ca8f088c17234b
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2025-09-28 22:08:08 +0530

    Move zero at last

--- Linked_list/LinkedList.java
+++ Linked_list/LinkedList.java
@@ -42,9 +42,10 @@ public class LinkedList {
         }
         Node temp=head;
         while(temp !=null){
-            System.out.print(temp.data+" ");
+            System.out.print(temp.data+"->");
             temp=temp.next;
         }
+        System.out.print("null");
 
     }
 
