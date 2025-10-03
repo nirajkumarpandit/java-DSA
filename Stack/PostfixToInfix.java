e407eadcf403cad0e6b1cf3b1c69d3d9d45e3e06
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2025-10-04 01:30:10 +0530

    postfix to infix conversion

---
+++ Stack/PostfixToInfix.java
@@ -0,0 +1,33 @@
+package Stack;
+import java.util.Stack;
+public class PostfixToInfix {
+      public static String postfixToInfix(String postfix) {
+        Stack<String> s = new Stack<>();
+        int n = postfix.length();
+
+        // Traverse the postfix expression from left to right
+        for (int i = 0; i < n; i++) {
+            char c = postfix.charAt(i);
+
+            // If the character is an operand, push it to the stack
+            if (Character.isLetterOrDigit(c)) {
+                s.push(String.valueOf(c));
+            } else {
+                // Pop two operands from the stack
+                String op2 = s.pop();
+                String op1 = s.pop();
+
+                // Form the new infix expression and push back to stack
+                s.push("(" + op1 + c + op2 + ")");
+            }
+        }
+
+        // The final element in the stack is the result
+        return s.peek();
+    }
+
+    public static void main(String[] args) {
+        String str="AB-DE+F*/";
+        System.out.println(postfixToInfix(str));
+    }
+}
