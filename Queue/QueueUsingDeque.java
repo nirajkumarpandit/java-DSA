c59accdbb3627f99628789656142f3ef07d1ebf3
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2025-10-20 10:56:19 +0530

    Stack and Queue using

---
+++ Queue/QueueUsingDeque.java
@@ -0,0 +1,32 @@
+package Queue;
+import java.util.*;
+public class QueueUsingDeque {
+    static class Queue {
+    
+        Deque<Integer> deque=new LinkedList<>();
+        // add
+         public void add(int data){
+            deque.addLast(data);
+         }
+         // remove
+         public int remove(){
+            return deque.removeFirst();
+         }
+         // peek
+         public int peek(){
+            return deque.getFirst();
+         }
+    }
+    public static void main(String[] args) {
+        Queue q=new Queue();
+        q.add(0);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println("peek ="+ q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    }
+}
