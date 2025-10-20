fb41f0a85ba0de8d4bae11f73e905b93a948b957
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2025-10-20 10:27:42 +0530

    Deque basic operation

---
+++ Queue/Double_ended_Queue.java
@@ -0,0 +1,14 @@
+package Queue;
+import java.util.*;
+public class Double_ended_Queue {
+    public static void main(String[] args) {
+        Deque<Integer> deque=new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addLast(4);
+        deque.addLast(5);
+        System.out.println(deque);
+        System.out.println(deque.getFirst());
+    }
+}
